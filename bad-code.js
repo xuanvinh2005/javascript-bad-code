ebea40dd54ea425ff5e1ca7a4f0daa228cbc9f32
vinh_lam <vlam@opswat.com>
2019-07-08 14:43:50 -0700

    .

--- bad-code.js
+++ bad-code.js
@@ -41,3 +41,24 @@ for(var i = 0; i < 10; i++){
 }               
 
 */
+
+var userData = {};
+
+// Fetch the location data for the current user.
+load();
+
+// Output the location of the user. Oops, it doesn't work! Why?
+console.log('Hello! Your IP address is ' + userData.ip + ' and your country is ' + userData.country_name);
+
+// The load function will detect the current visitor's ip and location with ajax, using the
+// freegeoip service. It will place the returned data in the userData variable when it's ready.
+
+function load(){
+
+    $.getJSON('https://freegeoip.net/json/?callback=?', function(response){
+        userData = response;
+
+        // Uncomment this line to see what is returned:
+        // console.log(response);
+    });
+}
